f42ca273f355132369875303cb95d326278fda6d
biran <blrbiran@163.com>
2020-02-16 14:41:00 +0800

    update golang settings in different PC

--- machine-related/.machine-related-MacOS-1-mac-mini-2014.zsh
+++ machine-related/.machine-related-MacOS-1-mac-mini-2014.zsh
@@ -1,3 +1,11 @@
+#!/bin/zsh
+
+# Golang related
+export GOROOT=/usr/local/go
+export GOPATH=$HOME/go
+export GOBIN=$GOPATH/bin
+export GO15VENDOREXPERIMENT=1
+export PATH=$PATH:$GOBIN
 
 # tabtab source for serverless package
 # uninstall by removing these lines or running `tabtab uninstall serverless`
@@ -8,3 +16,4 @@
 # tabtab source for slss package
 # uninstall by removing these lines or running `tabtab uninstall slss`
 [[ -f /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/slss.zsh ]] && . /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/slss.zsh
+
