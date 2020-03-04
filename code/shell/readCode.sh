985596400fd591d5dd4a38643beeb4700f694e29
blrbiran <blrbiran@163.com>
2020-03-04 10:08:40 +0800

    Update readCode.sh

--- code/shell/readCode.sh
+++ code/shell/readCode.sh
@@ -2,23 +2,23 @@
 
 bbcscope ()
 {
-	bbcg && \
-	PWD= /bin/pwd && \
-	local HERE=$PWD && \
-	cscope -Rbq -P $HERE
+    bbcg && \
+    PWD= /bin/pwd && \
+    local HERE=$PWD && \
+    cscope -Rbq -P $HERE
 }
 
 bbcurcscope ()
 {
-	PWD= /bin/pwd && \
-	local HERE=$PWD && \
-	cscope -Rbq -P $HERE
+    PWD= /bin/pwd && \
+    local HERE=$PWD && \
+    cscope -Rbq -P $HERE
 }
 
 bbctags ()
 {
-	bbcg && \
-	ctags -R .
+    bbcg && \
+    ctags -R .
 }
 
 bbcg ()
