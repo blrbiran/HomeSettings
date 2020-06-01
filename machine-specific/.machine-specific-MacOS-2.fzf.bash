afa802a88467836316f6c4568d3a1bcd9730769e
biran <blrbiran@163.com>
2020-06-02 01:15:56 +0800

    update fzf settings

--- machine-specific/.machine-specific-MacOS-2.fzf.bash
+++ machine-specific/.machine-specific-MacOS-2.fzf.bash
@@ -11,3 +11,8 @@ fi
 # Key bindings
 # ------------
 source "/usr/local/opt/fzf/shell/key-bindings.bash"
+
+# fzf settings
+export FZF_DEFAULT_COMMAND="fd --exclude={.git,.idea,.vscode,.sass-cache,node_modules,build} --type f"
+export FZF_DEFAULT_OPTS="--height 80% --layout=reverse --preview '(highlight -O ansi {} || cat {}) 2> /dev/null | head -500'"
+
