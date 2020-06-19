463e992b3e8c23f4e180a699476e97cc4e227f9b
mtk15011 <mtk15011@example.com>
2020-06-19 13:03:07 +0800

    remove useless configs

--- machine-specific/.machine-specific-Ubuntu.bash
+++ machine-specific/.machine-specific-Ubuntu.bash
@@ -1,6 +1,44 @@
 #!/bin/bash
 # === Place machine specific bash settings here.
 
+# == PS1 settings
+#PS1="[\u@\h \W]\$"
+prompt_git=y
+function last-command-prompt {
+	if [ $? == 0 ];then
+		printf "\[\e[1;32m\]➔ "
+	else
+		printf "\[\e[1;31m\]✘ "
+	fi
+}
+function git-branch-prompt {
+	if [ ${prompt_git} == "y" ];then
+		branch=$(git symbolic-ref --short -q HEAD 2>/dev/null)
+	fi
+	# if branch exist
+	if [ $branch ];then
+		status=$(git status -uno 2>/dev/null)
+		# git dirty status
+		if [[ ${status} =~ "nothing to commit" ]];then
+			state=""
+		elif [[ ${status} =~ "Changes to be committed" ]];then
+			state="\[\e[0;33m\] ●"
+		fi
+		if [[ ${status} =~ "Changes not staged for commit" ]];then
+			if [[ "${state}" == "" ]];then
+				state="\[\e[0;33m\] ✚"
+			else
+				state+="✚"
+			fi
+		fi
+		echo "\[\e[1;34m\](\[\e[0;33m\] \[\e[1;31m\]${branch}\[\e[1;34m\])${state}"
+	fi
+}
+function set-bash-prompt {
+	PS1="$(last-command-prompt)\[\e[1;36m\]\W$(git-branch-prompt)\[\e[0m\] "
+}
+PROMPT_COMMAND=set-bash-prompt
+
 # == PATH settings
 export PATH=~/code/shell:~/usr/bin:$PATH
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/usr/lib
--- machine-specific/.machine-specific-wsl.bash
+++ machine-specific/.machine-specific-wsl.bash
@@ -1,6 +1,44 @@
 #!/bin/bash
 # === Place machine specific bash settings here.
 
+# == PS1 settings
+#PS1="[\u@\h \W]\$"
+prompt_git=y
+function last-command-prompt {
+	if [ $? == 0 ];then
+		printf "\[\e[0;32m\]➔ "
+	else
+		printf "\[\e[0;31m\]✘ "
+	fi
+}
+function git-branch-prompt {
+	if [ ${prompt_git} == "y" ];then
+		branch=$(git symbolic-ref --short -q HEAD 2>/dev/null)
+	fi
+	# if branch exist
+	if [ $branch ];then
+		status=$(git status -uno 2>/dev/null)
+		# git dirty status
+		if [[ ${status} =~ "nothing to commit" ]];then
+			state=""
+		elif [[ ${status} =~ "Changes to be committed" ]];then
+			state="\[\e[0;33m\] ●"
+		fi
+		if [[ ${status} =~ "Changes not staged for commit" ]];then
+			if [[ "${state}" == "" ]];then
+				state="\[\e[0;33m\] ✚"
+			else
+				state+="✚"
+			fi
+		fi
+		echo "\[\e[0;34m\](\[\e[0;33m\] \[\e[0;31m\]${branch}\[\e[0;34m\])${state}"
+	fi
+}
+function set-bash-prompt {
+	PS1="$(last-command-prompt)\[\e[0;36m\]\W$(git-branch-prompt)\[\e[0m\] "
+}
+PROMPT_COMMAND=set-bash-prompt
+
 # == aliases and functions
 
 # == PATH settings
