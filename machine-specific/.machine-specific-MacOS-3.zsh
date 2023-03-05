033cd6e7a91d753a3f1a6a01fa5dbde7053bae88
biran <blrbiran@163.com>
2023-03-05 13:59:00 +0800

    update anaconda

--- machine-specific/.machine-specific-MacOS-3.zsh
+++ machine-specific/.machine-specific-MacOS-3.zsh
@@ -124,3 +124,18 @@ if [ -f '/Users/biran/tools/google-cloud-sdk/path.zsh.inc' ]; then . '/Users/bir
 # The next line enables shell command completion for gcloud.
 if [ -f '/Users/biran/tools/google-cloud-sdk/completion.zsh.inc' ]; then . '/Users/biran/tools/google-cloud-sdk/completion.zsh.inc'; fi
 
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/Users/biran/anaconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/Users/biran/anaconda3/etc/profile.d/conda.sh" ]; then
+        . "/Users/biran/anaconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/Users/biran/anaconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+
