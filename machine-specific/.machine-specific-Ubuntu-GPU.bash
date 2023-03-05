21f7ed44128c6d0f5e1f58e5cc548d91c5763c34
biran <blrbiran@163.com>
2023-03-05 13:44:29 +0800

    update anaconda

--- machine-specific/.machine-specific-Ubuntu-GPU.bash
+++ machine-specific/.machine-specific-Ubuntu-GPU.bash
@@ -74,3 +74,18 @@ export GOSUMDB=off
 [[ -s /usr/share/autojump/autojump.bash ]] && \
 . /usr/share/autojump/autojump.sh
 
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/home/biran/anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/home/biran/anaconda3/etc/profile.d/conda.sh" ]; then
+        . "/home/biran/anaconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/home/biran/anaconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+
