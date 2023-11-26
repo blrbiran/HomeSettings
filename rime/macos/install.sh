c91dd6fa0305b14e1c1c2e0d5e90def57c055063
biran <blrbiran@163.com>
2023-11-27 01:06:47 +0800

    Update RIME settings for MacOS

---
+++ rime/macos/install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+ln -sf ${PWD}/default.custom.yaml ~/Library/Rime/
+ln -sf ${PWD}/luna_pinyin.custom.yaml ~/Library/Rime/
+ln -sf ${PWD}/rime.lua ~/Library/Rime/
+ln -sf ${PWD}/lua ~/Library/Rime/
